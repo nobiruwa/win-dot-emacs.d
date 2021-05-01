6554fc805dbf86faa2b637a8132db0b4c5e580b3
ein <ein@hprei.localdomain>
2021-05-01 14:07:27 +0900

    update.

--- init.el
+++ init.el
@@ -406,7 +406,10 @@ See `expand-file-name'."
 (defvar user--latin-font "VL Gothic"
   "Default font for Latin characters")
 
-(defvar user--unicode-font "Noto Sans"
+(defvar user--cjk-proportional-font "VL PGothic"
+  "Default font for Latin characters")
+
+(defvar user--unicode-font "Noto Sans Mono CJK JP"
   "Default font for Unicode characters. including emojis")
 
 ;; Notoフォントでベンガル語(charset名はbengali)を表示するとクラッシュする。
@@ -418,12 +421,14 @@ See `expand-file-name'."
 (defvar user--unicode-font-fallback "FreeSans"
   "Fallback font for Unicode characters.")
 
-(defvar user--standard-fontset
-  (create-fontset-from-fontset-spec standard-fontset-spec)
+(defvar user--standard-fontset "fontset-user"
   "Standard fontset for user.")
 
 (defun user--set-font ()
   "Set Unicode, Latin and CJK font for user--standard-fontset."
+  ;; 記号にはデフォルトのフォントではなく指定のフォントを使いたい
+  (setq use-default-font-for-symbols nil)
+  (create-fontset-from-ascii-font user--cjk-font nil (replace-regexp-in-string "fontset-" "" user--standard-fontset))
   (set-fontset-font user--standard-fontset 'unicode
                     (font-spec :family user--unicode-font)
                     nil 'prepend)
@@ -434,24 +439,25 @@ See `expand-file-name'."
     (set-fontset-font user--standard-fontset charset
                   (font-spec :family user--cjk-font)
                   nil 'prepend))
+  ;; TODO 日本語フォントではU+2018とU+2019は全角幅だがWeb上の英文ではアポストロフィに使われていて
+  ;; 見栄えが悪い。現状は全角で表示し必要に応じてU+0027に置換する。よい方法はないものか。
   (dolist (charset '((#x2018 . #x2019)    ;; Curly single quotes "‘’"
                      (#x201c . #x201d)))  ;; Curly double quotes "“”"
     (set-fontset-font user--standard-fontset charset
                       (font-spec :family user--cjk-font)
-                      nil 'prepend))
+                      nil)) ; 上書きするために第5引数ADDは省略する
   ;; フォールバックフォントを用いる言語(charsetは C-u C-x = のscriptセクションの名前を用いる)
   (dolist (charset '(bengali bengali-akruti bengali-cdac))
     (set-fontset-font user--standard-fontset charset
                       (font-spec :family user--unicode-font-fallback)
                       nil 'prepend)))
+
+;; create fontset-user
 (user--set-font)
-(add-hook 'before-make-frame-hook #'user--set-font)
 
 ;; Ensure user--standard-fontset gets used for new frames.
-(add-to-list 'default-frame-alist (cons 'font user--standard-fontset))
-(add-to-list 'initial-frame-alist (cons 'font user--standard-fontset))
-;; Let the face 'fixed-pitch' use my fontset 'user--standard-fontset'.
-(set-face-attribute 'fixed-pitch nil :family 'unspecified)
+(add-to-list 'default-frame-alist `(font . ,user--standard-fontset))
+(add-to-list 'initial-frame-alist `(font . ,user--standard-fontset))
 
 ;;;
 ;; customize theme, color
